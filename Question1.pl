27ce1aeff3d618bfecbd5ff8c15c6e5d84a231ea
abejfehr <abe.fehr@gmail.com>
2014-04-08 19:46:18 -0400

    got a and c to work

--- Question1.pl
+++ Question1.pl
@@ -3,11 +3,29 @@
 % Abe Fehr
 % 100908743
 
-occurrences_of(X,Y,Z) :-
+% "base" case
+occurrences_of(_,[],0).
+occurrences_of(X, [X|Y], Z) :-
+  occurrences_of(X,Y,I),
+  Z is I+1.
+occurrences_of(X,[A|Y],Z) :-
+  not(X = A),
+  occurrences_of(X,Y,Z).
 
-flatten_list([],[]) :- !.
-flatten_list(X,Y) :-
+% "base" case
+flatten_list([], []).
+% I know that I'm doing something wrong right here, but I can't
+% figure out what. I think Imma give up for today, sorry guys.
+flatten_list([], _) :-
+  [].
+flatten_list([W|X],Y) :-
+  flatten_list(X,append(Y,W)).
 
-% I changed number and index to the proper math terms
-% base and exponent so I wouldn't get confused.
-power(Base,Exponent,Power) :-
+
+% I changed names to proper math terms
+% "base" and "exponent" so I wouldn't get confused.
+power(_, 0, 1). %the "base" case
+power(Base, Exponent, Power) :-
+  NewExponent is Exponent - 1, %decrement the exponent
+  NewPower    is Power * Base, %multiply the power
+  power(Base, NewExponent, NewPower). %call it again
